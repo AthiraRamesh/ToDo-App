adfebcd44e85a88968739e5ab7acead142eb9bba
AthiraRamesh <athirarameshm@gmail.com>
2023-09-09 15:42:57 +0530

    create add ToDo item section using stack widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,31 +12,80 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-                child: ListView(
+      body: Stack(
+        children: [
+          Container(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
               children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 50, bottom: 20),
-                  child: const Text(
-                    'All ToDos',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-                  ),
-                ),
-                for (ToDo todoo in todosList)
-                  ToDoItem(
-                    todo: todoo,
-                  ),
+                searchBox(),
+                Expanded(
+                    child: ListView(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 50, bottom: 20),
+                      child: const Text(
+                        'All ToDos',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    for (ToDo todoo in todosList)
+                      ToDoItem(
+                        todo: todoo,
+                      ),
+                  ],
+                ))
               ],
-            ))
-          ],
-        ),
-      )),
+            ),
+          )),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none),
+                  ),
+                )),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: tdBlue,
+                        minimumSize: Size(60, 60),
+                        elevation: 10),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
